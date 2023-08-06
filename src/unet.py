6ec169a4dbcd700a2841d471fe155c539a5419c4
Mahsa <khoshnoodi.ma@gmail.com>
2023-08-06 23:33:36 +0330

    add xunet class

--- src/unet.py
+++ src/unet.py
@@ -21,6 +21,22 @@ def kernel_and_same_pad(*kernel_size):
     paddings = tuple(map(lambda k: k//2, kernel_size))
     return dict(kernel_size = kernel_size, paddings=paddings)
 
+def cast_tuple(val, length=None):
+    if isinstance(val, list):
+        val = tuple(val)
+
+    output = val if isinstance(val, tuple) else ((val,) * default(length, 1))
+
+    return output
+def Upsample(dim, dim_out):
+    return nn.ConvTranspose3d(dim, dim_out, (1, 4, 4), (1, 2, 2), (0, 1, 1))
+
+def Downsample(dim, dim_out):
+    return nn.Sequential(
+        Rearrange('b c f (h s1) (w s2) -> b (c s1 s2) f h w', s1=2, s2=2),
+        nn.Conv3d(dim * 4, dim_out, 1)
+    )
+
 class Residual(nn.Module):
     def __init__(self, fn):
         super().__init__()
@@ -183,3 +199,141 @@ class TransformerBlock(nn.Module):
         x = self.attn(x)
         x = self.ff(x)
         return x
+
+class FeatureMapConsolidator(nn.Module):
+    def __init__(self,
+                 dim,
+                 *,
+                 dim_ins=tuple(),
+                 dim_outs=tuple(),
+                 resize_fmap_before=True,
+                 conv_block_fn=None
+            ):
+        super().__init__()
+        self.needs_consolidating = len(dim_ins) > 0
+
+        block_fn = default(conv_block_fn, Block)
+
+        self.fmap_convs = nn.ModuleList([block_fn(dim_in, dim_out) for dim_in, dim_out in zip(dim_ins, dim_outs)])
+        self.resize_fmap_before = resize_fmap_before
+
+        self.final_dim_out = dim + (sum(dim_outs) if len(dim_outs) > 0 else 0)
+
+    def resize_fmaps(self, fmaps, height, width):
+        return [F.interpolate(fmap, (fmap.shape[-3], height, width)) for fmap in fmaps]
+
+
+    def forward(self, x, fmaps=None):
+        target_height, target_width = x.shape[-2:]
+
+        fmaps = default(fmaps, tuple())
+
+        if not self.needs_consolidating:
+            return x
+
+        if self.resize_fmap_before:
+            fmaps = self.resize_fmaps(fmaps, target_height, target_width)
+
+        outs = []
+        for fmap, conv in zip(fmaps, self.fmap_convs):
+            outs.append(conv(fmap))
+
+        if self.resize_fmap_before:
+            outs = self.resize_fmaps(outs, target_height, target_width)
+
+        return torch.cat((x, *outs), dim = 1)
+
+    def kernel_and_same_pad(*kernel_size):
+        paddings = tuple(map(lambda k: k//2, kernel_size))
+        return dict(kernel_size = kernel_size, padding = paddings)
+
+
+class xunet(nn.Module):
+    def __init__(self,
+                 dim,
+                 init_dim=None,
+                 out_dim=None,
+                 frame_kernel_size=1,
+                 dim_mults=(1, 2, 4, 8),
+                 num_blocks_per_stage=(2, 2, 2, 2),
+                 num_self_attn_per_stage=(0, 0, 0, 1),
+                 nested_unet_depths=(0, 0, 0, 0),
+                 nested_unet_dim=32,
+                 channels=3,
+                 use_convnext=False,
+                 resnet_groups=8,
+                 consolidate_upsample_fmaps = True,
+                 skip_scale=2 ** -0.5,
+                 weight_standardize=False,
+                 attn_heads=8,
+                 attn_dim_head=32
+                 ):
+        super().__init__()
+
+        self.train_as_image = frame_kernel_size == 1
+
+        self.skip_scale = skip_scale
+        self.channels = channels
+
+        init_dim = default(init_dim, dim)
+        self.init_conv = nn.Conv3d(channels, init_dim, **kernel_and_same_pad(frame_kernel_size, 7, 7))
+
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+
+        num_resolutions = len(in_out)
+        blocks = partial(ConvNextBlock, frame_kernel_size=frame_kernel_size) if use_convnext else partial(ResnetBlock, groups=resnet_groups,
+                                                                                                          weight_standardize=weight_standardize,
+                                                                                 frame_kernel_size=frame_kernel_size)
+
+        nested_unet_depth = cast_tuple(nested_unet_depths, num_resolutions)
+
+        num_blocks_per_stage = cast_tuple(num_blocks_per_stage, num_resolutions)
+
+        num_self_attn_per_stage = cast_tuple(num_self_attn_per_stage, num_resolutions)
+
+        attn_heads = cast_tuple(num_self_attn_per_stage, num_resolutions)
+
+        attn_heads = cast_tuple(attn_heads, num_resolutions)
+        attn_dim_head = cast_tuple(num_self_attn_per_stage, num_resolutions)
+
+        skip_dims = []
+
+        down_stage_parameters = [
+            in_out,
+            nested_unet_depth,
+            num_blocks_per_stage,
+            num_self_attn_per_stage,
+            attn_heads,
+            attn_dim_head
+        ]
+
+        up_stage_parameters = [reversed(params[:-1]) for params in down_stage_parameters]
+
+        for ind, ((dim_in, dim_out), nested_unet_depth, num_blocks, self_attn_blocks, heads, dim_head) in enumerate(zip(*down_stage_parameters)):
+            is_last = ind >= (num_resolutions - 1)
+            skip_dims.append(dim_in)
+
+            self.downs.append(nn.ModuleList([
+                blocks(dim_in,
+                       dim_in,
+                       nested_unet_depth=nested_unet_depth,
+                       nested_unet_dim=nested_unet_dim,
+                       ),
+                nn.ModuleList([blocks(dim_in, dim_in, nested_unet_depth = nested_unet_depth, nested_unet_dim = nested_unet_dim) for _ in range(num_blocks - 1)]),
+                nn.ModuleList([TransformerBlock(dim_in, depth=self_attn_blocks, heads=heads, dim_head=dim_head) for _ in range(self_attn_blocks)]),
+                Downsample(dim_in, dim_out)
+            ]))
+
+        mid_dim = dims[-1]
+        mid_neted_unet_depth = nested_unet_depths[-1]
+
+        self.mid = blocks(mid_dim, mid_dim, nested_unet_depth=mid_neted_unet_depth, nested_unet_dim=nested_unet_dim)
+        self.mid_attn = Attention(mid_dim, heads = attn_heads[-1], dim_head = attn_dim_head[-1])
+        self.mid_after = blocks(mid_dim, mid_dim, nested_unet_depth = mid_nested_unet_depth, nested_unet_dim = nested_unet_dim)
+
+        self.mid_upsample = Upsample(mid_dim, dims[-2])
+
